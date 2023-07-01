28125043c870c28b3cfa2f689dd1860ffbe4cbe3
antonio <viz7mor@gmail.com>
2023-07-01 20:23:15 +0200

    Práctica 1

---
+++ script.js
@@ -0,0 +1,29 @@
+
+
+let Results = [
+      {
+          "title": "Estudiar Js",
+          "priority": "alta",
+          "isDone": false
+      },
+      {
+          "title": "Estudiar CSS",
+          "priority": "alta",
+          "isDone": true
+      },
+      {
+          "title": "Estudiar OOP",
+          "priority": "media",
+          "isDone": false
+      },
+      {
+          "title": "Estudiar IA",
+          "priority": "baja",
+          "isDone": false
+      }
+    ];
+
+console.log(Results[0].title);
+console.log(Results[1].title);
+console.log(Results[2].title);
+console.log(Results[3].title);
